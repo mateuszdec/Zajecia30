7932aa48c4600cbc2ddd7dd1536f093bd5b38825
Mateusz Dec <mateusz.dec92@gmail.com>
2020-01-21 18:09:08 +0100

    Zajecia 30 - json

--- src/main/java/org/javastart/demo/controller/UserController.java
+++ src/main/java/org/javastart/demo/controller/UserController.java
@@ -3,16 +3,22 @@ package org.javastart.demo.controller;
 import org.javastart.demo.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class UserController {
 
     @GetMapping("/users")
-    @ResponseBody
     public List<User> getUsers() {
         return List.of(new User("Marcin", 25), new User("Rafał", 27));
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable(name = "id") long id) {
+        return new User("User with id 1", 324);
     }
 }
